5e594a43d7ee849f67788d257f76b8aa9cc9b0c0
Daniel Gaiottino <daniel.gaiottino@gmail.com>
2011-07-12 10:09:11 +0200

    Added Bloomfilter module

--- lib/bloomfilter.rb
+++ lib/bloomfilter.rb
@@ -3,42 +3,33 @@
 require 'java'
 require 'ext/java-bloomfilter-0.9.3'
 
+require_relative 'bloomfilter/serializer'
+
 module Jar
   import com.skjegstad.utils.BloomFilter
 end
 
-class Bloomfilter
-  def initialize(options = {})
-    if options[:size] && options[:false_positive_percentage]
-      @filter = Jar::BloomFilter.new(options[:false_positive_percentage], options[:size])
-    elsif options[:filter]
-      @filter = options[:filter]
-    else
-      raise 'Initialize need either :filter or :size AND :false_positive_percentage'
+module Bloomfilter
+  class Bloomfilter
+    def initialize(options = {})
+      if options[:size] && options[:false_positive_percentage]
+        @filter = Jar::BloomFilter.new(options[:false_positive_percentage], options[:size])
+      elsif options[:filter]
+        @filter = options[:filter]
+      end
     end
-  end
   
-  def << (k)
-    @filter.add(k)
-  end
-  
-  def include?(k)
-    @filter.contains(k)
-  end
-  
-  def count
-    @filter.count
-  end
-  
-  def store(file_path)
-    File.open(file_path, 'w') do |file|
-      Marshal.dump(@filter, file)
+    def << (k)
+      @filter.add(k)
     end
-  end
   
-  def self.load(file_path)
-    File.open(file_path, 'r') do |file|
-      return new(:filter => Marshal.load(file))
+    def include?(k)
+      @filter.contains(k)
     end
+  
+    def count
+      @filter.count
+    end
+    
   end
 end
--- spec/bloomfilter_spec.rb
+++ spec/bloomfilter_spec.rb
@@ -4,51 +4,35 @@ require_relative 'spec_helper'
 
 require 'tempfile'
 
-describe Bloomfilter do
-  before :all do
-    @filter = Bloomfilter.new(:size => 100_000, :false_positive_percentage => 0.01)
-    @path = Tempfile.new('bloomfilter').path
-  end
-  
-  it 'should not have any elements from start' do
-    @filter.count.should == 0
-  end
-  
-  describe '#<<' do
-    it 'should be possible to add elements which updates the count' do
-      @filter << 'hello'
-      @filter.count.should == 1
+module Bloomfilter
+  describe Bloomfilter do
+    before :all do
+      @filter = Bloomfilter.new(:size => 1_000, :false_positive_percentage => 0.01)
+      @path = Tempfile.new('bloomfilter').path
     end
-  end
   
-  describe '#include?' do
-    it 'should return false if an element does not exist' do
-      @filter.include?('world').should be_false
+    it 'should not have any elements from start' do
+      @filter.count.should == 0
     end
+  
+    describe '#<<' do
+      it 'should be possible to add elements which updates the count' do
+        @filter << 'hello'
+        @filter.count.should == 1
+      end
+    end
+  
+    describe '#include?' do
+      it 'should return false if an element does not exist' do
+        @filter.include?('world').should be_false
+      end
     
-    it 'should return true if an element exists' do
-      @filter.include?('hello').should be_true
+      it 'should return true if an element exists' do
+        @filter.include?('hello').should be_true
       
-      @filter << 'world'
-      @filter.include?('world').should be_true
-    end
-  end
-  
-  context 'serialization' do
-    before do
-      @filter.store(@path)
-    end
-    
-    it 'should be possible to store a filter to a file' do
-      File.new(@path).size.should == 126552
-    end
-    
-    it 'should be possible to load a filter from a file' do
-      filter = Bloomfilter.load(@path)
-      filter.count.should == 2
-      filter.include?('hello').should be_true
-      filter.include?('world').should be_true
-      filter.include?('bloomfilter').should be_false
+        @filter << 'world'
+        @filter.include?('world').should be_true
+      end
     end
   end
 end
